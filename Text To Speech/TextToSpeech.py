981fdf546041a1f0667b70a0098871000224395f
Sardar Ahmed <sardarahmednazar@gmail.com>
2022-12-07 18:38:09 +0500

    done

--- Text To Speech/TextToSpeech.py
+++ Text To Speech/TextToSpeech.py
@@ -1,10 +1,16 @@
 import os
 from gtts import gTTS
 
-text = "Hello! I am Ahmed"
+#text = "Hello! I am Ahmed"
 
-output = gTTS(text = text, lang='en', slow= False)
-output.save('output.mp3')
+#output = gTTS(text = text, lang='en', slow= False)
+#output.save('output.mp3')
 
 
-os.system("start output.mp3")
+#os.system("start output.mp3")
+
+text = open('demo.txt', 'r').read()
+output = gTTS(text=text, lang='en', slow=False)
+output.save('fileOutput.mp3')
+
+os.system("start fileOutput.mp3")
